41a08c2e6182efe0be31aa7c9e685af921294312
vs4vijay <vs4vijay@gmail.com>
2020-09-05 13:40:15 +0530

    add pino logger

--- src/app.js
+++ src/app.js
@@ -2,33 +2,36 @@
 'use strict';
 
 const express = require('express');
+const logger = require('pino')();
+
 const config = require('./config');
+
 
 const app = express();
 
-
 app.use(express.json());
+
 
 // Root Route
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Parking Lot System is Running'
-    });
+  res.json({
+    message: 'Parking Lot System is Running',
+  });
 });
 
 // Handle 404 Routes
 app.get('*', (req, res) => {
-    res.status(404).json({
-        message: 'Resource not found'
-    });
+  res.status(404).json({
+    message: 'Resource not found',
+  });
 });
 
-if(require.main == module) {
-    app.listen(config.PORT, () => {
-        console.log(`Service has started on port ${config['PORT']}`);
-    })
+if (require.main == module) {
+  app.listen(config.PORT, () => {
+    logger.info(`Service has started on port ${config['PORT']}`);
+  });
 }
 
-process.on('SIGINT', function() {
-    process.exit();
+process.on('SIGINT', function () {
+  process.exit();
 });
